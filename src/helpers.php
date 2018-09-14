26b3f4339e93988aa543ad60bf1c4965cea4ff29
wangyongtao <wangtom365@qq.com>
2018-09-14 17:14:57 +0800

    update

--- src/helpers.php
+++ src/helpers.php
@@ -225,4 +225,42 @@ if (!function_exists('get_rand_prize')) {
         return $result;
     }
 }
+
+if (!function_exists('get_client_ip')) {
+    /**
+     * 获取客户ip
+     *
+     * @return string
+     */
+    function get_client_ip()
+    {
+        $ip = '';
+        if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+            $arr = explode(",", $_SERVER["HTTP_X_FORWARDED_FOR"]);
+            $pos = array_search("unknown", $arr);
+            if (false !== $pos) {
+                unset($arr[$pos]);
+            }
+            $ip = trim($arr[0]);
+        } elseif (isset($_SERVER["HTTP_CLIENT_IP"])) {
+            $ip = $_SERVER["HTTP_CLIENT_IP"];
+        } elseif (isset($_SERVER["REMOTE_ADDR"])) {
+            $ip = $_SERVER["REMOTE_ADDR"];
+        }
+
+        return ($ip && ip2long($ip) !== false) ? $ip : '';
+    }
+}
+
+if (!function_exists("get_referer_url")) {
+    /**
+     * 获取来源地址
+     *
+     * @return string
+     */
+    function get_referer_url()
+    {
+        return isset($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : '';
+    }
+}
 // --------------------------------------------------------------------------
--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -11,22 +11,76 @@ final class HelperTest extends TestCase
         $this->assertTrue(true);
     }
 
-     public function testFormatPrice()
-     {
-         $price = '5.3456';
-         $result = \Cnwyt\Helpers\format_price($price);
-         var_dump($result);
+    public function testFormatPrice()
+    {
+        $price = '5.3456';
+        $result = \Cnwyt\Helpers\format_price($price);
+        $this->assertEquals('5.30', $result);
+        $this->assertEquals('5.30', $result);
 
-         $this->assertEquals('5.30', $result);
-         $this->assertEquals('5.30', $result);
+        $price = '6.68';
+        $result = \Cnwyt\Helpers\format_price($price);
+        $this->assertEquals('6.7', $result);
 
-         $price = '6.68';
-         $result = \Cnwyt\Helpers\format_price($price);
-         var_dump($result);
+        $price = '12346';
+        $result = \Cnwyt\Helpers\format_price($price);
+        $this->assertEquals('12346', $result);
+    }
 
-         $price = '12346';
-         $result = \Cnwyt\Helpers\format_price($price);
-         var_dump($result);
-     }
+    public function testIsPhoneNumber()
+    {
+        $this->assertFalse(\Cnwyt\Helpers\is_phone_number('12345678901'));
+        $this->assertFalse(\Cnwyt\Helpers\is_phone_number('02182838284'));
+        $this->assertTrue(\Cnwyt\Helpers\is_phone_number('18513622082'));
+        $this->assertTrue(\Cnwyt\Helpers\is_phone_number('19513622082'));
+        $this->assertTrue(\Cnwyt\Helpers\is_phone_number('14913622082'));
+    }
+
+    public function testIsValidUrlAndEmail()
+    {
+        $str = 'cnwyt@outlook.com';
+        $result = \Cnwyt\Helpers\is_valid_email($str);
+        $this->assertTrue($result);
+
+        $str = 'cnwyt@outlookcom';
+        $result = \Cnwyt\Helpers\is_valid_email($str);
+        $this->assertFalse($result);
+
+        $str = 'https://wx.wang123.net/useragent?src=test';
+        $result = \Cnwyt\Helpers\is_valid_url($str);
+        $this->assertTrue($result, 'ERROR: the url is valid.');
+
+        $str = 'wx.wang123.net/useragent?src=test';
+        $result = \Cnwyt\Helpers\is_valid_url($str);
+        $this->assertFalse($result, 'ERROR: the url is invalid.');
+    }
+
+    public function testGetFuzzyName()
+    {
+        $str = 'cnwyt@outlook.com';
+        $result = \Cnwyt\Helpers\get_fuzzy_name($str);
+        $this->assertEquals('cn***om', $result);
+
+        $str = '会飞的小鸟';
+        $result = \Cnwyt\Helpers\get_fuzzy_name($str);
+        $this->assertEquals('会飞***小鸟', $result);
+
+        $str = '宋江';
+        $result = \Cnwyt\Helpers\get_fuzzy_name($str);
+        $this->assertEquals('宋***', $result);
+    }
+
+
+    public function testGetRandPrize()
+    {
+        $prizeArr = [
+            0 => ['name' => '未中奖', 'probability' => '50'],
+            1 => ['name' => '积分', 'probability' => '200'],
+            2 => ['name' => '50元购物卡', 'probability' => '300'],
+            3 => ['name' => '100元优惠券', 'probability' => '400'],
+        ];
+        $result = \Cnwyt\Helpers\get_rand_prize($prizeArr);
+        $this->assertGreaterThan(0, $result);
+    }
 
 }
